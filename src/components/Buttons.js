f43590cfbd53fbdc8959c4136740169c3e0381c7
William Kwong <work.williamkwong@gmail.com>
2018-05-21 14:08:02 -0400

    Additional formatting

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 
 class Buttons extends React.Component {
-    
+    render() {
+        return (
+            <div class="text-center">
+                <Router>
+                    <div class="btn-group" role="group" aria-label="">
+                        <a class="btn btn-outline-primary" href="https://drive.google.com/uc?export=download&id=1bsw19G31J2ZvSHAMPRJxUpdpJdGM6vUh" role="button">Download Resume</a>
+                        <a class="btn btn-outline-primary" href="https://www.linkedin.com/in/w5kwong/" role="button">Linkedin</a>
+                        <a class="btn btn-outline-primary" href="https://github.com/Work-WilliamKwong" role="button">Github</a>
+                    </div>
+                </Router>
+            </div>
+        )
+    }
 }
+
+export default Buttons;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Buttons from './Buttons';
 
 class Header extends React.Component {
     render() {
         return (
             <div class="jumbotron">
-                <h1 class="text-center">William Kwong</h1>
-                <div class="container">
-                    <p class="text-center">Software developer and avid coffee lover</p>
-                    <p class="text-center">1B Mechatronics Engineering</p>
+                <h1 class="text-center display-1 margin-2">William Kwong</h1>
+                <div class="my-3 container">
+                    <h5 class="text-center">150 Shadow Falls Drive, Richmond Hill, Canada</h5>
+                    <h5 class="text-center">647-983-3751</h5>
+                    <h5 class="text-center">w5kwong@edu.uwaterloo.ca</h5>
                 </div>
-            </div>
+                <div class="my-5"></div>
+                <Buttons />
+            </div>  
         )
     }
 }
