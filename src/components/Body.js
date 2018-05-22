f9b383935e9d18570800f069a886caa5e6a4ab79
William Kwong <work.williamkwong@gmail.com>
2018-05-21 20:26:44 -0400

    Additional content to body

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,11 +6,20 @@ class Body extends React.Component {
             <div class="container">
                 <div class="row justify-content-start">
                     <div class="col-lg">
-                        <p class="text-right">I'm a first year engineering student with a passion for writing software and managing projects.</p>
-                        <p class="text-right">I have experience in full stack application development with technologies such as React.js, PostgreSQL, and Apache Kafka.</p>
+                        <p class="text-left">I'm a first year engineering student with a passion for writing software and project management.</p>
+                        <p class="text-left">I have experience in full stack application development with technologies such as React.js, PostgreSQL, and Apache Kafka.</p>
+                        <p class="text-left">Things I like to do outside of work include Ultimate Frisbee and reading. My favorite book currently is Principles by Ray Dalio.</p>
                     </div>
                     <div class="col-lg">
-                        <p>I also really like coffee.</p>
+                        <h5>Skills</h5>
+                        <ul>
+                            <li>React.js</li>
+                            <li>PostgreSQL</li>
+                            <li>Ruby</li>
+                            <li>C++</li>
+                            <li>Javascript</li>
+                            <li>Unity</li>
+                        </ul>
                     </div>
                 </div>
             </div>
