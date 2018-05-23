fb25297c2fe50105e6daeb28157fa17d2e3f5c57
William Kwong <work.williamkwong@gmail.com>
2018-05-22 20:58:47 -0400

    New resume address

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,7 +9,7 @@ class Body extends React.Component {
                         <p class="text-left">I'm a first year engineering student at the University of Waterloo with a passion for writing software and project management.</p>
                         <p class="text-left">I have experience in full stack application development with technologies such as React.js, PostgreSQL, and Apache Kafka.</p>
                         <p class="text-left">Things I like to do include ultimate frisbee, photography, reading, and meeting new people!</p>
-                        <p class="text-left">Shoot me an email if you'd like to grab coffee! I'm always looking to find interesting and new perspectives from people I've never met.</p>
+                        <p class="text-left">Shoot me an text/email if you'd like to grab coffee! I'm always looking to find interesting and new perspectives from people I've never met.</p>
                     </div>
                     <div class="col-lg">
                         <h5>Skills</h5>
@@ -21,6 +21,7 @@ class Body extends React.Component {
                             <li>Java</li>
                             <li>Javascript</li>
                             <li>AWS</li>
+                            <li>Docker</li>
                             <li>HashiCorp</li>
                             <li>Apache Kafka</li>
                             <li>Unity</li>
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,7 +7,7 @@ class Buttons extends React.Component {
             <div class="text-center">
                 <Router>
                     <div class="btn-group" role="group" aria-label="">
-                        <a class="btn btn-outline-primary" href="https://drive.google.com/uc?export=download&id=1bsw19G31J2ZvSHAMPRJxUpdpJdGM6vUh" role="button">Download Resume</a>
+                        <a class="btn btn-outline-primary" href="https://drive.google.com/uc?export=download&id=1c3TA7eIeRDx7n9yD5yj2XnuC8EmHc7ZW" role="button">Download Resume</a>
                         <a class="btn btn-outline-primary" href="https://www.linkedin.com/in/w5kwong/" role="button">LinkedIn</a>
                         <a class="btn btn-outline-primary" href="https://github.com/Work-WilliamKwong" role="button">GitHub</a>
                         <a class="btn btn-outline-primary" href="https://www.flickr.com/photos/158359269@N02/" role="button">Flickr</a>
