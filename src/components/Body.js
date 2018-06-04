5b58cdc83ecdab13d4c168c499d14cab18ae919d
William Kwong <work.williamkwong@gmail.com>
2018-06-03 20:10:16 -0400

    Changes to content

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,18 +6,18 @@ class Body extends React.Component {
             <div class="container">
                 <div class="row justify-content-start">
                     <div class="col-lg">
-                        <p class="text-left">I'm a first year engineering student at the University of Waterloo with a passion for writing software and project management.</p>
+                        <p class="text-left">I'm a first year mechatronics engineering student at the University of Waterloo with a passion for writing software and project management.</p>
                         <p class="text-left">I have experience in full stack application development with technologies such as React.js, PostgreSQL, and Apache Kafka.</p>
-                        <p class="text-left">Things I like to do include ultimate frisbee, photography, reading, and meeting new people!</p>
-                        <p class="text-left">Shoot me an text/email if you'd like to grab coffee! I'm always looking to find interesting and new perspectives from people I've never met.</p>
+                        <p class="text-left">Things I like to do include ultimate frisbee, photography, reading, and snowboarding.</p>
+                        <p class="text-left">Shoot me an text/email for any inquiries or if you'd like to grab coffee!</p>
                     </div>
                     <div class="col-lg">
-                        <h5>Skills</h5>
+                        <h5>Technical Skills</h5>
                         <ul>
                             <li>React.js</li>
                             <li>PostgreSQL</li>
                             <li>Ruby</li>
-                            <li>C++</li>
+                            <li>C/C++</li>
                             <li>Java</li>
                             <li>Javascript</li>
                             <li>AWS</li>
