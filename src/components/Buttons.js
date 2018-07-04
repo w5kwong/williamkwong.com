2b491ee3d9d8595b42b52835860aa8198e96ec70
William Kwong <work.williamkwong@gmail.com>
2018-07-03 21:43:34 -0400

    Updates to resume

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -7,7 +7,7 @@ class Buttons extends React.Component {
             <div class="text-center">
                 <Router>
                     <div class="btn-group" role="group" aria-label="">
-                        <a class="btn btn-outline-primary" href="https://drive.google.com/uc?export=download&id=1Vmaf8dEnsIxcz5v1ORdBJL0rmuOcbEWm" role="button">Download Resume</a>
+                        <a class="btn btn-outline-primary" href="https://drive.google.com/uc?export=download&id=1hMBvpo3yXuwtlMHv1sCaFItCWpmHrsUU" role="button">Download Resume</a>
                         <a class="btn btn-outline-primary" href="https://www.linkedin.com/in/w5kwong/" role="button">LinkedIn</a>
                         <a class="btn btn-outline-primary" href="https://github.com/Work-WilliamKwong" role="button">GitHub</a>
                         <a class="btn btn-outline-primary" href="https://www.flickr.com/photos/158359269@N02/" role="button">Flickr</a>
