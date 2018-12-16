2573e9a928e6a388bc18acdcf6237f4e3367f749
William Kwong <work.williamkwong@gmail.com>
2018-12-15 20:20:54 -0500

    Compressed images, updated body formating

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,36 +5,44 @@ import robot from '../images/robot.jpg';
 
 class Body extends React.Component {
     render() {
+        const cardStyle = {
+            backgroundColor: '#e9ecef'
+        };
         return (
             <div class="container">
                 <div class="mb-4">
                     <h3>Work Experience</h3>
                 </div>
-                <div class="row justify-content-start mb-4">
-                    <div class="col">
-                        <img src={autodesk} class="img-fluid mx-auto d-block pic" alt="Responsive image"></img>
+                <div class="row mb-4">
+                    <div class="col py-5 mx-2 my-2" style={cardStyle}>
+                        <img src={autodesk} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Responsive image"></img>
+                        <h5 class="text-center mt-4">CTO Research Intern - Autodesk</h5>
                     </div>
-                    <div class="col">
-                        <img src={marketcircle} class="img-fluid mx-auto d-block pic" alt="Responsive image"></img>
+                    <div class="col py-5 mx-2 my-2" style={cardStyle}>
+                        <img src={marketcircle} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Responsive image"></img>
+                        <h5 class="text-center mt-4">Backend Software Developer - Marketcircle</h5>
                     </div>
-                    <div class="col">
-                        <img src={robot} class="img-fluid mx-auto d-block pic" alt="Responsive image"></img>
+                    <div class="w-100"></div>
+                    <div class="col py-5 mx-2 my-2" style={cardStyle}>
+                        <img src={robot} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Responsive image"></img>
+                        <h5 class="text-center mt-4">Software Developer Intern - Robot Playtime</h5>
                     </div>
+                    <div class="col py-5 mx-2 my-2"></div>
                 </div>
                 {/* <ul class="timeline">
-                    <li>
-                        <h5>Autodesk</h5>
-                        <p></p>
-                    </li>
-                    <li>
-                        <h5>Marketcircle</h5>
-                        <p></p>
-                    </li>
-                    <li>
-                        <h5>Robot Playtime</h5>
-                        <p></p>
-                    </li>
-                </ul> */}
+                        <li>
+                            <h5>Autodesk</h5>
+                            <p></p>
+                        </li>
+                        <li>
+                            <h5>Marketcircle</h5>
+                            <p></p>
+                        </li>
+                        <li>
+                            <h5>Robot Playtime</h5>
+                            <p></p>
+                        </li>
+                    </ul> */}
             </div>
         )
     }
