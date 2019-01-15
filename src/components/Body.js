fd32ce9018420a3d189420f06fc62fefce68d781
William Kwong <work.williamkwong@gmail.com>
2019-01-15 13:10:36 -0500

    Changes to body layout

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,21 +14,43 @@ class Body extends React.Component {
                     <h3>Work Experience</h3>
                 </div>
                 <div class="row mb-4">
-                    <div class="col-md py-5 mx-2 my-2" style={cardStyle}>
+                    <div class="col-md py-4 mx-3 my-2" style={cardStyle}>
                         <img height="300px" width="300px" src={autodesk} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Autodesk Logo"></img>
-                        <h5 class="text-center mt-4">CTO Research Intern - Autodesk</h5>
+                        <h4 class="text-center mt-4">Autodesk</h4>
+                        <h5 class="text-center">Research Intern, CTO</h5>
                     </div>
-                    <div class="col-md py-5 mx-2 my-2" style={cardStyle}>
+                    <div class="col-md py-4 mx-3 my-2" style={cardStyle}>
                         <img src={marketcircle} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Marketcircle Logo"></img>
-                        <h5 class="text-center mt-4">Backend Software Developer - Marketcircle</h5>
+                        <h4 class="text-center mt-4">Marketcircle</h4>
+                        <h5 class="text-center">Backend Software Developer</h5>
                     </div>
-                    <div class="w-100"></div>
-                    <div class="col-md py-5 mx-2 my-2" style={cardStyle}>
+                    {/* <div class="w-100"></div> */}
+                    <div class="col-md py-4 mx-3 my-2" style={cardStyle}>
                         <img src={robot} class="img-fluid mx-auto d-block pic img-thumbnail" alt="Robot Playtime Logo"></img>
-                        <h5 class="text-center mt-4">Software Developer Intern - Robot Playtime</h5>
+                        <h4 class="text-center mt-4">Robot Playtime</h4>
+                        <h5 class="text-center">Software Developer Intern</h5>
                     </div>
-                    <div class="col-md py-5 mx-2 my-2"></div>
+                    {/* <div class="col-md py-5 mx-2 my-2"></div> */}
                 </div>
+
+                {/* <div class="mb-4">
+                    <h3>Skills</h3>
+                </div>
+                <div class="row mb-4">
+                    <div class="col-md py-4 mx-3 my-2">
+                        <h4 class="text-center">Languages</h4>
+                        <div class="py-2 mx-3 my-2" style={cardStyle}>
+                            <ul class="text-center">
+                                <li class="text-center">C/C++</li>
+                                <li class="text-center">Javascript</li>
+                                <li class="text-center">Java</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md py-4 mx-3 my-2"></div>
+                    <div class="col-md py-4 mx-3 my-2"></div>
+                </div> */}
+
                 {/* <ul class="timeline">
                         <li>
                             <h5>Autodesk</h5>
